e956adc50373cac394ce465057b8175269dfd03f
Le An Ha <halean@yahoo.com>
2024-01-22 17:38:33 +0000

    Evening22012024

--- src/components/MathExercise.js
+++ src/components/MathExercise.js
@@ -38,7 +38,7 @@ function MathExercise({exercise}) {
     return (
     <div class="max-w-3xl bg-white p-6 rounded shadow">
     <a id={exercise.trinket}><h1 class="text-2xl font-bold mb-4">{exercise.title}</h1></a>
-    <p class="mb-4">{exercise.content}</p>
+    <p class="mb-4">{parse(exercise.content)}</p>
     <iframe src={exercise.further_reading_and_exercises} width="100%" height="800" frameborder="0" marginwidth="0" marginheight="0" allowfullscreen/>
     </div>
     )
